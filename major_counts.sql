a4cf723530af8f5cb901f379c446f7f135af1b05
Jason P. Browning <jpaulbrowning@gmail.com>
2020-02-20 18:08:43 -0700

    Add second major and remove aggregation
    This version uses a UNION to retrieve both the primary and secondary major, and includes both in the final major counts.  Removed aggregation; the query instead returns a list of student PIDMs.

--- major_counts.sql
+++ major_counts.sql
@@ -1,5 +1,6 @@
 WITH enrolled_students AS (
-        SELECT DISTINCT a.sfrstcr_term_code,
+        SELECT DISTINCT
+               a.sfrstcr_term_code,
                a.sfrstcr_pidm
           FROM sfrstcr a
          WHERE a.sfrstcr_camp_code <> 'XXX'
@@ -8,34 +9,64 @@ WITH enrolled_students AS (
                  (SELECT b.stvrsts_code
                     FROM stvrsts b
                    WHERE b.stvrsts_incl_sect_enrl = 'Y'))
-    SELECT b.sfrstcr_term_code AS term_code,
-           d.stvterm_desc AS term_desc,
-           a.sgbstdn_levl_code,
-           a.sgbstdn_coll_code_1,
-           a.sgbstdn_degc_code_1,
-           a.sgbstdn_majr_code_1,
-           c.stvmajr_desc AS major_desc,
-           COUNT(a.sgbstdn_pidm) AS student_cnt
-      FROM sgbstdn a
-INNER JOIN enrolled_students b
-        ON a.sgbstdn_pidm = b.sfrstcr_pidm
- LEFT JOIN stvmajr c
-        ON a.sgbstdn_majr_code_1 = c.stvmajr_code
- LEFT JOIN stvterm d
-        ON b.sfrstcr_term_code = d.stvterm_code
-     WHERE a.sgbstdn_stst_code = 'AS'
-       AND a.sgbstdn_coll_code_1 = 'FA'
-       AND SUBSTR(b.sfrstcr_term_code,1,4) BETWEEN '2014' AND '2019'
-       AND SUBSTR(b.sfrstcr_term_code,5,2) = '40'
-       AND a.sgbstdn_term_code_eff =
-           (SELECT MAX(aa.sgbstdn_term_code_eff)
-              FROM sgbstdn aa
-             WHERE a.sgbstdn_pidm = aa.sgbstdn_pidm
-               AND aa.sgbstdn_term_code_eff <= b.sfrstcr_term_code)
-  GROUP BY b.sfrstcr_term_code,
-           d.stvterm_desc,
-           a.sgbstdn_levl_code,
-           a.sgbstdn_coll_code_1,
-           a.sgbstdn_degc_code_1,
-           a.sgbstdn_majr_code_1,
-           c.stvmajr_desc;
+
+    SELECT f.sfrstcr_term_code   AS term_code,
+           g.stvterm_desc,
+           f.sgbstdn_levl_code   AS levl_code,
+           h.stvlevl_desc,
+           f.sgbstdn_coll_code_1 AS coll_code,
+           j.stvcoll_desc,
+           f.sgbstdn_degc_code_1 AS degc_code,
+           k.stvdegc_desc,
+           f.sgbstdn_majr_code_1 AS majr_code,
+           l.stvmajr_desc,
+           f.sgbstdn_pidm        AS student_pidm
+     FROM (
+              SELECT b.sfrstcr_term_code,
+                     a.sgbstdn_levl_code,
+                     a.sgbstdn_coll_code_1,
+                     a.sgbstdn_degc_code_1,
+                     a.sgbstdn_majr_code_1,
+                     a.sgbstdn_pidm
+                FROM sgbstdn a
+          INNER JOIN enrolled_students b
+                  ON a.sgbstdn_pidm = b.sfrstcr_pidm
+               WHERE a.sgbstdn_stst_code = 'AS'
+                 AND SUBSTR(b.sfrstcr_term_code, 1, 4) BETWEEN '2014' AND '2019'
+                 AND SUBSTR(b.sfrstcr_term_code, 5, 2) = '40'
+                 AND a.sgbstdn_term_code_eff = (SELECT MAX(aa.sgbstdn_term_code_eff)
+                                                  FROM sgbstdn aa
+                                                 WHERE a.sgbstdn_pidm = aa.sgbstdn_pidm
+                                                   AND aa.sgbstdn_term_code_eff <= b.sfrstcr_term_code)
+
+              UNION ALL
+
+              SELECT d.sfrstcr_term_code,
+                     b.sgbstdn_levl_code,
+                     b.sgbstdn_coll_code_2,
+                     b.sgbstdn_degc_code_2,
+                     b.sgbstdn_majr_code_2,
+                     b.sgbstdn_pidm
+                FROM sgbstdn b
+          INNER JOIN enrolled_students d
+                  ON b.sgbstdn_pidm = d.sfrstcr_pidm
+               WHERE b.sgbstdn_stst_code = 'AS'
+                 AND SUBSTR(d.sfrstcr_term_code, 1, 4) BETWEEN '2014' AND '2019'
+                 AND SUBSTR(d.sfrstcr_term_code, 5, 2) = '40'
+                 AND b.sgbstdn_term_code_eff = (SELECT MAX(bb.sgbstdn_term_code_eff)
+                                                  FROM sgbstdn bb
+                                                 WHERE b.sgbstdn_pidm = bb.sgbstdn_pidm
+                                                   AND bb.sgbstdn_term_code_eff <= d.sfrstcr_term_code)
+          ) f
+LEFT JOIN stvterm g
+       ON f.sfrstcr_term_code = g.stvterm_code
+LEFT JOIN stvlevl h
+       ON f.sgbstdn_levl_code = h.stvlevl_code
+LEFT JOIN stvcoll j
+       ON f.sgbstdn_coll_code_1 = j.stvcoll_code
+LEFT JOIN stvdegc k
+       ON f.sgbstdn_degc_code_1 = k.stvdegc_code
+LEFT JOIN stvmajr l
+       ON f.sgbstdn_majr_code_1 = l.stvmajr_code
+
+
