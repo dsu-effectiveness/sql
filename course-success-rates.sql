53f3e41df34b3a28941d342627ce2a7095eaf4c0
Jason P. Browning <jpaulbrowning@gmail.com>
2020-03-09 18:57:00 -0600

    Added instructor type (FT v. PT)
    Pulls instructor type from PEBEMPL table, uses primary instructor from SIRASGN.

--- course-success-rates.sql
+++ course-success-rates.sql
@@ -4,11 +4,15 @@
            a.ssbsect_crse_numb,
            a.ssbsect_seq_numb,
            e.gtvinsm_desc,
+           CASE WHEN g.pebempl_ecls_code IN ('F2','F9') THEN 'FT'
+                ELSE 'PT'
+                END AS instructor_type,
            COUNT(DISTINCT c.sfrstcr_pidm) AS enrl_cnt,
            SUM(CASE WHEN c.sfrstcr_grde_code IN ('A+','A','A-') THEN 1
                     WHEN c.sfrstcr_grde_code IN ('B+','B','B-') THEN 1
                     WHEN c.sfrstcr_grde_code IN ('C+','C','S')  THEN 1
-                    ELSE 0 END) AS success_cmpltd
+                    ELSE 0
+                    END) AS success_cmpltd
       FROM ssbsect a
 INNER JOIN stvterm b
         ON a.ssbsect_term_code = b.stvterm_code
@@ -19,10 +23,16 @@ INNER JOIN stvrsts d
         ON c.sfrstcr_rsts_code = d.stvrsts_code
  LEFT JOIN gtvinsm e
         ON a.ssbsect_insm_code = e.gtvinsm_code
+ LEFT JOIN sirasgn f
+        ON a.ssbsect_term_code = f.sirasgn_term_code
+       AND a.ssbsect_crn = f.sirasgn_crn
+ LEFT JOIN pebempl g
+        ON f.sirasgn_pidm = g.pebempl_pidm
      WHERE a.ssbsect_ssts_code = 'A'
        AND a.ssbsect_term_code = '201940'
        AND a.ssbsect_enrl > 0
        AND d.stvrsts_incl_sect_enrl = 'Y'
+       AND f.sirasgn_primary_ind = 'Y'
        -- Erin's list of courses; these could be removed to generalize.
        AND CONCAT(a.ssbsect_subj_code, a.ssbsect_crse_numb) IN (
              'ECON1740','HIST1700','HIST2700','HIST2710','POLS1100',
@@ -33,4 +43,8 @@ INNER JOIN stvrsts d
            a.ssbsect_subj_code,
            a.ssbsect_crse_numb,
            a.ssbsect_seq_numb,
-           e.gtvinsm_desc;
+           f.sirasgn_pidm,
+           e.gtvinsm_desc,
+           CASE WHEN g.pebempl_ecls_code IN ('F2','F9') THEN 'FT'
+                ELSE 'PT'
+                END;
